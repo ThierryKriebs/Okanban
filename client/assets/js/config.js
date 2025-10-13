6dc166d3bc78413c844bacd510ab02451bd19659
ThierryKriebs <kriebs.thierry@gmail.com>
2025-10-13 19:15:42 +0200

    test

--- client/assets/js/config.js
+++ client/assets/js/config.js
@@ -1,2 +1,3 @@
-export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+//export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+export const apiBaseUrl = "https://vps-fb2758c8.vps.ovh.net/api";
 
