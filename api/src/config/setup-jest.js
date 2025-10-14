16f7ea4fb3d4f4a456c1f57722b1b3dd429178e9
ThierryKriebs <kriebs.thierry@gmail.com>
2025-10-14 12:59:11 +0200

    testsauto

--- api/src/config/setup-jest.js
+++ api/src/config/setup-jest.js
@@ -1,11 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.test' }); // Charge .env.test
+
 import { sequelize } from '../models/index.js';
+
+// Crée une instance dédiée aux tests
+export const testSequelize = new Sequelize(process.env.PG_URL, {
+    define: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+    logging: false,
+  });
+
+
 import { populateTests } from '../migrations/populateTests.js';
 
 beforeEach(async () => {
-    await sequelize.sync({ force: true });
-    await populateTests();
+    // await sequelize.sync({ force: true });
+    // await populateTests();
+    await testSequelize.sync({ force: true });
+    await populateTests({ sequelize: testSequelize }); // Passe l'instance de test
 });
 
 afterAll(async () => {
-    await sequelize.close();
+    // await sequelize.close();
+    await testSequelize.close();
 });
